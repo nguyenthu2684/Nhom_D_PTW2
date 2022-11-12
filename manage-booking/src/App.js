78b6e251f1b0e540db083538f6b2e02de53c7c2a
ngocyen02 <ngocyentxrt021201@gmail.com>
2022-11-13 04:10:59 +0700

    update home by yen

--- manage-booking/src/App.js
+++ manage-booking/src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import HeaderComponent from './component/header/header';
+import FooterComponent from './component/footer/footer';
+import HomePage from './pages/home/home';
 function App() {
   return (
   
     <>
     <HeaderComponent/>
+    <HomePage/>
+    <FooterComponent/>
     </>
   );
 }
--- manage-booking/src/component/header/header.js
+++ manage-booking/src/component/header/header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './header.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
-import logo from './img/logo.png';
+import logo from '../img/logo.png';
 function HeaderComponent  ()  {
     return (
         <>
