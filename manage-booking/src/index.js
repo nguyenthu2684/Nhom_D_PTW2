90b15b51f9152a239f970a976a5d96e999958a49
ngocyen02 <ngocyentxrt021201@gmail.com>
2022-11-29 10:15:36 +0700

    update

--- manage-booking/src/index.js
+++ manage-booking/src/index.js
@@ -5,8 +5,8 @@ import './styles/styles.scss';
 
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
-import IntlProviderWrapper from "./hoc/IntlProviderWrapper";
 
+import IntlProviderWrapper from './hoc/IntlProviderWrapper';
 
 import { Provider } from 'react-redux';
 import reduxStore, { persistor } from './redux';
